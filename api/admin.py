9da0fb7d32f0f345c537e22034c2e01b8b8653a4
Denis, Antonov <den69ant@yandex.ru>
2022-10-26 13:43:00 +0300

    models has been created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TypeNews, NewsItem
+
+
+admin.site.register(TypeNews)
+admin.site.register(NewsItem)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class TypeNews(models.Model):
+    COLORS = (
+        ('RED', 'RED'),
+        ('BLUE', 'BLUE'),
+        ('GREEN', 'GREEN'),
+        ('PINK', 'PINK'),
+        ('WHITE', 'WHITE'),
+        ('BLACK', 'BLACK'),
+    )
+    
+    name = models.CharField(max_length=150)
+    color = models.CharField(max_length=200, choices=COLORS)
+    
+    class Meta:
+        verbose_name_plural = "News Types"
+    
+    def __str__(self):
+        return self.name
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=100)
+    about = models.CharField(max_length=250)
+    description = models.TextField()
+    type_news = models.ForeignKey(TypeNews, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = "News Items"
+    
+    def __str__(self):
+        return self.title
+    
+
+
+
+
