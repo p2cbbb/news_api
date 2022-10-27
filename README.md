a598ccc3f6d90b0121e6b96bc168d76fd8e52714
Denis, Antonov <den69ant@yandex.ru>
2022-10-27 12:09:30 +0300

    update README.md, env variables added

--- README.md
+++ README.md
@@ -19,3 +19,27 @@
 - Возможность получить список всех типов новостей   
 - Возможность получить список всех новостей (имя, краткое описание, имя типа, цвет типа)    
 - Возможность получить список новостей определенного типа   
+
+### Копирование репозитория и установка зависимостей
+```bash
+git clone https://github.com/p2cbbb/news_api
+cd news_api
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Устанавливаем переменные окружения
+В папке `config` создать файл `.env` и заполнить eго ключами
+
+```bash
+SECRET_KEY=<secret_key>
+DEBUG=<debug>
+```
+
+### Применение миграций, создания суперпользователя и запуск проекта
+```bash
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+```
--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
+
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d5aku2yyg^^&qwyl)q-o74+^py#32kzjxmy&+)ao)nxuvy%7=#'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
